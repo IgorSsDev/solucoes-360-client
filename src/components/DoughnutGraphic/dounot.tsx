60a2ad0dd6aa53474d789c1f02797ce0f8c187cd
Igordevz <programadorigor.dev@gmail.com>
2024-03-04 15:38:56 -0300

    finalizando grafico

--- src/components/DoughnutGraphic/dounot.tsx
+++ src/components/DoughnutGraphic/dounot.tsx
@@ -1,35 +1,28 @@
-"use client"
-import React from 'react'
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
+"use client";
+import React, { useState } from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Bar } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 export default function DoughnutPage({ isData }: any) {
 
+  const index = isData.dataClient.company[1];
 
   const data = {
-    labels: ['Satisfeito', 'Neutro', 'Insatisfeito'],
+    labels: index?.history,
     datasets: [
       {
-        label: 'Satisfação dos seus clientes',
-        data: [isData.dataClient.satisfied[0].notas, isData.dataClient.satisfied[1].notas, isData.dataClient.satisfied[2].notas],
-        backgroundColor: [
-          'rgba(119, 0, 255, 0.2)',
-          'rgba(180, 214, 67, 0.2)',
-          'rgba(235, 54, 54, 0.2)',
-        ],
-        borderColor: [
-          '#8000ff',
-          'rgba(255, 206, 86, 1)',
-          '#ff0000',
-        ],
-        borderWidth: 1,
+        fill: true,
+        data: index?.range,
+        label: `pergunta: ${index?.pergunta}`,
+        borderColor: "#002fff",
+        backgroundColor: "#002fff",
       },
     ],
   };
   return (
     <div>
-      <Doughnut data={data} />
+      <Bar data={data} />
     </div>
-  )
+  );
 }
